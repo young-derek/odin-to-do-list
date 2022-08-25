61ff69cfb14be0eaf47dde64be2ea50eed48792f
Derek <derekyoung_8@msn.com>
2022-08-24 22:55:57 -0400

    Create add new project modal file

---
+++ src/new-project-modal.js
@@ -0,0 +1,11 @@
+const createModal = () => {
+    const body = document.querySelector('body');
+    const modal = document.createElement('div');
+    const modalTitle = document.createElement('h2');
+    const projectName = document.createElement('input');
+    const projectNameLabel = document.creatElement('input');
+    const btnSubmitProject = document.createElement('button');
+    
+    
+    
+}
--- src/page-layout.js
+++ src/page-layout.js
@@ -2,6 +2,7 @@ export const createPageLayout = () => {
     // MAIN CONTAINER
     const body = document.querySelector('body');
     const mainContainer = document.createElement('div');
+    mainContainer.id = 'main-container';
 
     // HEADER SECTION
     const header = document.createElement('header');
@@ -14,26 +15,31 @@ export const createPageLayout = () => {
     const projectsList = document.createElement('ul');
     const btnAddProject = document.createElement('button');
     projectsTitle.textContent = 'Projects:';
+    projectsTitle.classList.add('section-title');
+    projectsSection.id = 'projects-section';
+    btnAddProject.id = 'btn-add-project';
+    btnAddProject.textContent = '+ Add New Project';
 
     // TASKS SECTION
     const tasksSection = document.createElement('section');
     const tasksTitle = document.createElement('h2');
     const btnAddTask = document.createElement('button');
-    const tasksList = document.createElement('li');
-    tasksTitle.textContent = 'Tasks';
+    const tasksList = document.createElement('ul');
+    tasksTitle.classList.add('section-title');
+    tasksTitle.textContent = 'Tasks:';
+    tasksSection.id = 'tasks-section';
+    btnAddTask.id = 'btn-add-task';
+    btnAddTask.textContent = '+ Add New Task';
 
     // FOOTER SECTION
     const footer = document.createElement('footer');
     const copyrightInfo = document.createElement('p');
 
-
     // APPEND SECTION
     body.append(mainContainer);
     mainContainer.append(header, projectsSection, tasksSection, footer);
     header.append(pageTitle);
-    projectsSection.append(projectsTitle, projectsList, btnAddProject);
-    tasksSection.append(tasksTitle, tasksList, btnAddTask);
+    projectsSection.append(projectsTitle, btnAddProject, projectsList);
+    tasksSection.append(tasksTitle, btnAddTask, tasksList);
     footer.append(copyrightInfo);
-
-
 };
