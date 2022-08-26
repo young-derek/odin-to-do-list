f332d8a46652d9a7a0c0e99cf258f2cae090165b
Derek <derekyoung_8@msn.com>
2022-08-26 19:04:54 -0400

    Add display of projects to the DOM

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import './style.css';
-import {createPageLayout} from './page-layout.js';
+import { createPageLayout } from './page-layout.js';
+import { addEventListeners } from './event-listeners';
+import { Task } from './task';
+import { Project } from './project';
+import { refreshProjectsDisplay } from './utility';
 
-createPageLayout();
+((run) => {
+    createPageLayout();
+    addEventListeners();
+    const toDoList = [
+        Project('Default Project', 2022 - 10 - 10, [], true),
+    ];
+    const projectsList = document.querySelector('#projects-list');
+    const tasksList = document.querySelector('#tasks-list');
+
+
+    refreshProjectsDisplay(toDoList, projectsList);
+})();
