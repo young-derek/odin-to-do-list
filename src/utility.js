fb119817e442e1f1ea5a308319840a04d76198dc
Derek <derekyoung_8@msn.com>
2022-08-26 19:06:57 -0400

    Add project display and array functions

--- src/utility.js
+++ src/utility.js
@@ -7,7 +7,19 @@ export const toggleModal = (modal) => {
 };
 
 // function to refresh list of projects
+export const refreshProjectsDisplay = (projectsArray, projectsList) => {
+    projectsArray.forEach(project => {
+        const listItem = document.createElement('li')
+        listItem.textContent = project.title;
+        projectsList.append(listItem);
+    })
+}
 
 // function to display project specific tasks
 
 // function to display all tasks
+
+// function to append to array
+export const pushToArray = (array, item) => {
+    array.push(item);
+}
