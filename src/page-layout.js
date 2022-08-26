4b8dc269d594d30e37e02534b52b698dd4e5d45e
Derek <derekyoung_8@msn.com>
2022-08-26 12:24:04 -0400

    Add modal to layout

--- src/page-layout.js
+++ src/page-layout.js
@@ -1,3 +1,5 @@
+import { toggleModal } from './toggle-input-modal';
+
 export const createPageLayout = () => {
     // MAIN CONTAINER
     const body = document.querySelector('body');
@@ -35,11 +37,41 @@ export const createPageLayout = () => {
     const footer = document.createElement('footer');
     const copyrightInfo = document.createElement('p');
 
+    // MODAL SECTION
+    const modal = document.createElement('div');
+    const modalTitle = document.createElement('h2');
+    const modalTextInputContainer = document.createElement('div');
+    const modalTextInput = document.createElement('input');
+    const modalTextLabel = document.createElement('label');
+    const modalDateInputContainer = document.createElement('div');
+    const modalDateInput = document.createElement('input');
+    const modalDateLabel = document.createElement('label');
+    const btnModalSubmit = document.createElement('button');
+    const btnModalCancel = document.createElement('button');
+
+    modalTitle.textContent = 'new project/task';
+    btnModalSubmit.textContent = 'submit';
+    btnModalCancel.textContent = 'cancel';
+
+    modal.id = 'modal';
+    modalDateInput.id = 'modal-date-input';
+    modalTextInput.id = 'modal-text-input';
+    btnModalSubmit.id = 'btn-modal-submit';
+    btnModalCancel.id = 'btn-modal-cancel';
+
+    modalTextInput.setAttribute('type', 'text');
+    modalTextLabel.setAttribute('for', 'modal-text-input');
+    modalDateInput.setAttribute('type', 'date')
+    modalDateLabel.setAttribute('for', 'modal-date-input');
+
     // APPEND SECTION
     body.append(mainContainer);
-    mainContainer.append(header, projectsSection, tasksSection, footer);
+    mainContainer.append(header, projectsSection, tasksSection, footer, modal);
     header.append(pageTitle);
     projectsSection.append(projectsTitle, btnAddProject, projectsList);
     tasksSection.append(tasksTitle, btnAddTask, tasksList);
+    modalTextInputContainer.append(modalTextLabel, modalTextInput);
+    modalDateInputContainer.append(modalDateLabel, modalDateInput);
+    modal.append(modalTitle, modalTextInputContainer, modalDateInputContainer, btnModalSubmit, btnModalCancel);
     footer.append(copyrightInfo);
 };
