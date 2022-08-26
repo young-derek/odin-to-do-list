6f3d2ae5ca83f4bfa1ca81db8285f5a0f9e838bd
Derek <derekyoung_8@msn.com>
2022-08-26 16:09:02 -0400

    Create utility file for functions, add togglemodal

---
+++ src/utility.js
@@ -0,0 +1,13 @@
+// functions for event listeners
+
+// Toggle modal visibility
+export const toggleModal = (modal) => {
+    modal.classList.toggle('inactive');
+    modal.reset();
+};
+
+// function to refresh list of projects
+
+// function to display project specific tasks
+
+// function to display all tasks
