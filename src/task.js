b3dba4f7940aa4fb891637917b0ff602fdb7160e
Derek <derekyoung_8@msn.com>
2022-08-26 20:09:49 -0400

    Add documentation to task factory function

--- src/task.js
+++ src/task.js
@@ -1,3 +1,11 @@
+/**
+ * 
+ * @param {string} title Task title
+ * @param {date} dueDate Task due date
+ * @param {string} priority Task priority level
+ * @param {string} notes Additional notes about the task
+ * @returns New task object
+ */
 export const Task = (title, dueDate, priority, notes) => {
     return {
         title: title,
