304aad49a062e635490d0c8c0403c31576301e0b
Derek <derekyoung_8@msn.com>
2022-08-27 19:40:14 -0400

    Add task completion status to task objects

--- src/task.js
+++ src/task.js
@@ -4,6 +4,7 @@
  * @param {date} dueDate Task due date
  * @param {string} priority Task priority level
  * @param {string} notes Additional notes about the task
+ * @param {boolean} complete Task completion status
  * @returns New task object
  */
 export const Task = (title, dueDate, priority, notes) => {
@@ -12,5 +13,6 @@ export const Task = (title, dueDate, priority, notes) => {
         dueDate: dueDate,
         priority: priority,
         notes: notes,
+        complete: false,
     };
 };
